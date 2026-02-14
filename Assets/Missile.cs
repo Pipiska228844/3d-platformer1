448d23a9a84fedd186a0958316231d416b966645
Floxikwt <katk16022@gmail.com>
2026-02-14 20:08:21 +0800

    k

---
+++ Assets/Missile.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{ 
+    public float speed = 10f;
+    // Start is called before the first frame updat
+    void Start()
+    {
+    Destroy(gameObject, 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+{
+    transform.position +=
+    transform.forward *
+    speed * Time.deltaTime;
+}
+
+   void OntriggerEnter(Collider other) {
+    Destroy(gameObject);
+    Enemy enemy = other.GetComponent<Enemy>();
+    Destroy(enemy);
+
+
+    
+}
+
+}
